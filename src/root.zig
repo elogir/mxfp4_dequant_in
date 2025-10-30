1634c3474f5be7c687af2abfb0ce0f6e959e6c9b
rigole <rigole@duck.com>
2025-10-30 12:49:04 +0900

    print tensors infos

--- src/root.zig
+++ src/root.zig
@@ -1,11 +1,40 @@
 const std = @import("std");
+const json = std.json;
 
-pub fn dequant_tensor(allocator: std.mem.Allocator, tensor_reader: *std.Io.Reader) !void {
+fn parse_header(allocator: std.mem.Allocator, tensor_reader: *std.Io.Reader) !void {
     const header_size: usize = @intCast(try tensor_reader.takeInt(u64, .little));
     const header_data = try allocator.alloc(u8, header_size);
     defer allocator.free(header_data);
 
     try tensor_reader.readSliceAll(header_data);
 
-    std.debug.print("Taille header {}\nContent: {s}\n", .{ header_size, header_data });
+    const parsed = try json.parseFromSlice(json.Value, allocator, header_data, .{});
+    defer parsed.deinit();
+
+    const header = parsed.value.object;
+    var iter = header.iterator();
+    while (iter.next()) |entry| {
+        const tensor_name = entry.key_ptr.*;
+        const tensor_info = entry.value_ptr.*;
+
+        if (std.mem.eql(u8, tensor_name, "__metadata__")) continue;
+        std.debug.print("name: {s} | info: {}\n", .{ tensor_name, @TypeOf(tensor_info) });
+
+        const dtype = tensor_info.object.get("dtype").?.string;
+        const shape = tensor_info.object.get("shape").?.array;
+        // const data_offsets = tensor_info.object.get("data_offsets").?.array;
+
+        std.debug.print("\nTensor: {s}\n", .{tensor_name});
+        std.debug.print("  dtype: {s}\n", .{dtype});
+        std.debug.print("  shape: [", .{});
+        for (shape.items, 0..) |dim, i| {
+            if (i > 0) std.debug.print(", ", .{});
+            std.debug.print("{}", .{dim.integer});
+        }
+        std.debug.print("]\n", .{});
+    }
+}
+
+pub fn dequant_tensor(allocator: std.mem.Allocator, tensor_reader: *std.Io.Reader) !void {
+    try parse_header(allocator, tensor_reader);
 }
